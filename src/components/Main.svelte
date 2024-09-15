741c92429a9ba8e1d5b296d76acb97ce5f965835
Braden kravitz <bradenkravitz@Bradens-MacBook-Pro.local>
2024-09-14 21:14:16 -0400

    Updated profile picture

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -18,7 +18,7 @@
     <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 sm:py-14">
         <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Hi! I'm <span class="poppins text-violet-400">Braden Kravitz</span><br/> MIS
+                Hi! I'm <span class="poppins text-violet-400">Braden Kravitz</span><br/> Management Information Systems
                 <span class="poppins text-violet-400">Student</span> @ <span class="poppins text-violet-400">Penn State</span>
             </h2>
             <p class="text-base sm:text-lg md:text-xl">
